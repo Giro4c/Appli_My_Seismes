96bb2301c2286957be15fcb0e053ccc55dcb660b
Giro4c <camille.girodengo@etu.univ-amu.fr>
2023-06-07 20:55:37 +0200

    Major Update : updated pom.xml for dependencies + added repositories + added require in module-info pricipal

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,7 +5,8 @@ module fr.amu.iut.prototype1.appli_my_seismes {
         requires org.controlsfx.controls;
             requires com.dlsc.formsfx;
                     requires org.kordamp.bootstrapfx.core;
-            
+    requires com.gluonhq.maps;
+
     opens fr.amu.iut.prototype1.appli_my_seismes to javafx.fxml;
     exports fr.amu.iut.prototype1.appli_my_seismes;
 }
