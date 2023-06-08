d6da00cd82c1c20b00d7a971724d6ca478a148a5
Giro4c <camille.girodengo@etu.univ-amu.fr>
2023-06-08 11:39:40 +0200

    Upt: Classe Utilitaire.java + UtilitaireTest.java + richterColor() method

--- src/main/java/fr/amu/iut/prototype1/appli_my_seismes/Test.java
+++ src/main/java/fr/amu/iut/prototype1/appli_my_seismes/Test.java
@@ -11,7 +11,7 @@ public class Test {
         //testEqualsInteger();
         //testStringSplitForTime();
         //testStringSplitForDate();
-        testCSVReader();
+        //testCSVReader();
 
     }
 
