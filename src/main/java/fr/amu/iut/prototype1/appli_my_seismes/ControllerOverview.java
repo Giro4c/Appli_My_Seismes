f6bb36d6efa39b1dabea3926f0e9146d64fba656
moundir213 <bcho0407@gmail.com>
2023-06-10 03:14:04 +0200

    controller overview, + ouverture sur controllerSeisme, + fxml (vide)

--- src/main/java/fr/amu/iut/prototype1/appli_my_seismes/ControllerOverview.java
+++ src/main/java/fr/amu/iut/prototype1/appli_my_seismes/ControllerOverview.java
@@ -7,20 +7,19 @@ import javafx.stage.Stage;
 
 import java.util.ArrayList;
 
-public class Overview extends Application {
+public class ControllerOverview {
+
+//    private ControllerSeisme controllerSeisme;
 
     private static ArrayList<Seisme> listeSeisme = CSVReader.StringArrayToSeismeArrayList(
             CSVReader.CSVFileReader("src/main/resources/fr/amu/iut/prototype1/appli_my_seismes/SisFrance_seismes_20230604151458.csv"));
 
-    @Override
-    public void start(Stage primaryStage) {
-
-        VBox root = new VBox();
 
 
-        primaryStage.setScene(new Scene(root));
-        primaryStage.setTitle("Overview");
-        primaryStage.show();
-    }
+
+
+//    public void setControllerSeisme (ControllerSeisme controllerSeisme){
+//        this.controllerSeisme = controllerSeisme;
+//    }
 
 }
--- src/main/java/fr/amu/iut/prototype1/appli_my_seismes/ControllerSeisme.java
+++ src/main/java/fr/amu/iut/prototype1/appli_my_seismes/ControllerSeisme.java
@@ -8,10 +8,16 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class ControllerSeisme {
@@ -142,6 +148,26 @@ public class ControllerSeisme {
         tableView.getItems().setAll(listSeismesPage);
 
     }
+
+    //Overview
+    @FXML
+    private void OuvreOverview(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Overview.fxml"));
+        Parent root = loader.load();
+
+//        ControllerOverview controllerOverview = loader.getController();
+//        controllerOverview.setControllerSeisme(this);
+
+
+        Stage stage = new Stage();
+        stage.setTitle("Overview");
+        stage.setScene(new Scene(root));
+
+
+        stage.initOwner(((Node) event.getSource()).getScene().getWindow());
+        stage.show();
+    }
+
 
 
 }
