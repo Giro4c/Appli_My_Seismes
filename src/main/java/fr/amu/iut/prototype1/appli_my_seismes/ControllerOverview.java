7d1dfc6604adab541adee52131b61e00564c81fe
moundir213 <bcho0407@gmail.com>
2023-06-10 17:02:04 +0200

    3 graphes rajoutés : 1er onglet: nombres de seismes par regions ( camembert), 2eme onglet: moyenne d'intensité par region (1er graphe : 5premier, 2eme graphe : 5dernier)

--- src/main/java/fr/amu/iut/prototype1/appli_my_seismes/ControllerOverview.java
+++ src/main/java/fr/amu/iut/prototype1/appli_my_seismes/ControllerOverview.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXML;
 import javafx.scene.chart.*;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,22 +15,28 @@ public class ControllerOverview {
     private static ArrayList<Seisme> listeSeisme = CSVReader.StringArrayToSeismeArrayList(
             CSVReader.CSVFileReader("src/main/resources/fr/amu/iut/prototype1/appli_my_seismes/SisFrance_seismes_20230604151458.csv"));
     @FXML
-    private BarChart<String, Number> barChart;
+    private BarChart<String, Number> mostAffectedChart;
 
     @FXML
-    private CategoryAxis xAxis;
+    private BarChart<String, Number> leastAffectedChart;
 
     @FXML
-    private NumberAxis yAxis;
+    private CategoryAxis xAxisMostAffected;
+
+    @FXML
+    private NumberAxis yAxisMostAffected;
+
+    @FXML
+    private CategoryAxis xAxisLeastAffected;
+
+    @FXML
+    private NumberAxis yAxisLeastAffected;
+
 
     @FXML
     private PieChart Pie;
 
     public void initialize() {
-
-   //barchart
-
-
 
         // Créer une liste pour stocker les régions épicentrales
         List<String> regionsEpicentrales = listeSeisme.stream()
@@ -37,30 +44,59 @@ public class ControllerOverview {
                 .distinct()
                 .collect(Collectors.toList());
 
-// Créer une liste pour stocker les moyennes d'intensité épicentrale par région
-        List<Double> moyennesIntensiteEpicentrale = regionsEpicentrales.stream()
-                .map(region -> listeSeisme.stream()
-                        .filter(seisme -> seisme.getRegion().equals(region))
-                        .mapToDouble(Seisme::getIntensite)
-                        .average()
-                        .orElse(0.0)) // Valeur par défaut si la moyenne n'est pas calculée
+
+        //barchart
+
+
+        List<String> regionsByMostAffected = listeSeisme.stream()
+                .collect(Collectors.groupingBy(Seisme::getRegion, Collectors.averagingDouble(Seisme::getIntensite)))
+                .entrySet().stream()
+                .sorted(Comparator.comparingDouble(entry -> -entry.getValue()))
+                .limit(5)
+                .map(entry -> entry.getKey())
+                .collect(Collectors.toList());
+
+        // Trier les régions par intensité de séisme (du moins touché au plus touché)
+        List<String> regionsByLeastAffected = listeSeisme.stream()
+                .collect(Collectors.groupingBy(Seisme::getRegion, Collectors.averagingDouble(Seisme::getIntensite)))
+                .entrySet().stream()
+                .sorted(Comparator.comparingDouble(entry -> entry.getValue()))
+                .limit(5)
+                .map(entry -> entry.getKey())
                 .collect(Collectors.toList());
 
 
+        // Créer les séries de données pour les graphiques
+        XYChart.Series<String, Number> mostAffectedSeries = new XYChart.Series<>();
+        XYChart.Series<String, Number> leastAffectedSeries = new XYChart.Series<>();
 
-
-        // Crée une série de données à partir des listes xData et yData
-        XYChart.Series<String, Number> series = new XYChart.Series<>();
-        for (int i = 0; i < regionsEpicentrales.size(); i++) {
-            series.getData().add(new XYChart.Data<>(regionsEpicentrales.get(i), moyennesIntensiteEpicentrale.get(i)));
+        // Ajouter les données aux séries de données
+        for (String region : regionsByMostAffected) {
+            mostAffectedSeries.getData().add(new XYChart.Data<>(region, getAverageIntensityByRegion(listeSeisme, region)));
         }
 
-        // Ajoute la série de données au BarChart
-        barChart.getData().add(series);
+        for (String region : regionsByLeastAffected) {
+            leastAffectedSeries.getData().add(new XYChart.Data<>(region, getAverageIntensityByRegion(listeSeisme, region)));
+        }
+
+        // Ajouter les séries de données aux graphiques
+        mostAffectedChart.getData().add(mostAffectedSeries);
+        leastAffectedChart.getData().add(leastAffectedSeries);
+
+        // Définir les axes des graphiques
+        mostAffectedChart.setCategoryGap(20);
+        mostAffectedChart.setBarGap(5);
+        mostAffectedChart.setLegendVisible(false);
+        mostAffectedChart.setTitle("Intensité moyenne des Régions les plus touchées");
+
+        leastAffectedChart.setCategoryGap(20);
+        leastAffectedChart.setBarGap(5);
+        leastAffectedChart.setLegendVisible(false);
+        leastAffectedChart.setTitle("Intensité moyenne des Régions les moins touchées");
 
 
-        //pie
 
+        //PIE
 
 
         // Créer une liste pour stocker le nombre de séismes par région
@@ -70,24 +106,85 @@ public class ControllerOverview {
                         .count())
                 .collect(Collectors.toList());
 
-        // Créer les données pour le diagramme circulaire (camembert)
+        // Définir un seuil pour la catégorie "autres" des régions moins fréquentes
+        long seuil = 80; // Seuil
+
+        // Créer une liste pour stocker les données du diagramme circulaire (camembert)
         ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
+
+        // Parcourir les régions et le nombre de séismes correspondant
         for (int i = 0; i < regionsEpicentrales.size(); i++) {
             String region = regionsEpicentrales.get(i);
             long nombreSeismes = nombreSeismesParRegion.get(i);
-            pieChartData.add(new PieChart.Data(region, nombreSeismes));
+
+            // Vérifier si le nombre de séismes est inférieur au seuil
+            if (nombreSeismes < seuil) {
+                // Ajouter le nombre de séismes à la catégorie "Autres"
+                PieChart.Data autresData = pieChartData.stream()
+                        .filter(data -> data.getName().equals("Autres"))
+                        .findFirst()
+                        .orElse(null);
+
+                if (autresData == null) {
+                    autresData = new PieChart.Data("Autres", nombreSeismes);
+                    pieChartData.add(autresData);
+                } else {
+                    autresData.setPieValue(autresData.getPieValue() + nombreSeismes);
+                }
+            } else {
+
+                //pourcentage
+                double pourcentage = (nombreSeismes / (double) getTotalSeismes(nombreSeismesParRegion)) * 100;
+
+                // Ajouter la région et le pourcentage dans le texte de la légende
+                PieChart.Data data = new PieChart.Data(region + " (" + String.format("%.2f", pourcentage) + "%)", nombreSeismes);
+                pieChartData.add(data);
+
+
+
+
+            }
+
+
+            // Définir les données du Pie
+            Pie.setData(pieChartData);
+
         }
 
-        // Créer le diagramme circulaire (camembert)
-        Pie.setData(pieChartData);
-        Pie.setTitle("Nombre de séismes par région");
+
+
+
 
 
     }
 
 
 
+    // Méthode pour calculer le total des séismes
+    private long getTotalSeismes(List<Long> nombreSeismesParRegion) {
+        return nombreSeismesParRegion.stream()
+                .mapToLong(Long::longValue)
+                .sum();
     }
+
+
+    private double getAverageIntensityByRegion(List<Seisme> listeSeisme, String region) {
+        // Calculer la moyenne d'intensité pour une région donnée
+        List<Seisme> seismesByRegion = listeSeisme.stream()
+                .filter(seisme -> seisme.getRegion().equals(region))
+                .collect(Collectors.toList());
+
+        double sumIntensity = seismesByRegion.stream()
+                .mapToDouble(Seisme::getIntensite)
+                .sum();
+
+        return sumIntensity / seismesByRegion.size();
+    }
+
+
+
+
+}
 
 
 
