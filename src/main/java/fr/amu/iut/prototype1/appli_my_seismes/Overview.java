5ec173b9b98bfb0138a021fd52a284d62c55cbd4
Giro4c <camille.girodengo@etu.univ-amu.fr>
2023-06-11 23:40:57 +0200

    Upt: petite modif doc overview appli

--- src/main/java/fr/amu/iut/prototype1/appli_my_seismes/Overview.java
+++ src/main/java/fr/amu/iut/prototype1/appli_my_seismes/Overview.java
@@ -9,6 +9,8 @@ import java.util.ArrayList;
 
 /**
  * L'application représentative de la fenetre Overview qui contient des données statistiques sur la liste de séismes traitée par l'application.
+ * @deprecated Remplacée par une nouvelle fenetre dans MainControler
+ * @see MainControler#showOverview()
  */
 public class Overview extends Application {
 
