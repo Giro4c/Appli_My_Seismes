3eca7857dd25a625f9d654f18c098146d79a6cbb
Giro4c <camille.girodengo@etu.univ-amu.fr>
2023-06-12 15:02:15 +0200

    Upt: Suppr Widgets car never used et will never be used

--- src/main/java/fr/amu/iut/prototype1/appli_my_seismes/Widget.java
+++
@@ -1,7 +0,0 @@
-package fr.amu.iut.prototype1.appli_my_seismes;
-
-import javafx.scene.layout.Pane;
-
-public class Widget extends Pane {
-
-}
--- src/main/java/fr/amu/iut/prototype1/appli_my_seismes/WidgetGraphique.java
+++
@@ -1,4 +0,0 @@
-package fr.amu.iut.prototype1.appli_my_seismes;
-
-public class WidgetGraphique extends Widget {
-}
